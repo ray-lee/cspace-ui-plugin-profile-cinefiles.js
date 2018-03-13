4b1fd91946a9f74214d80a2cfa86d04c903173e6
Cesar Villalobos <cesarv.h@berkeley.edu>
2018-03-13 02:34:23 -0700

    BAMPFA-585: Hid authorities that are not needed

--- src/index.js
+++ src/index.js
@@ -25,5 +25,59 @@ module.exports = () => ({
       version: cspaceUIPluginProfileCineFiles.packageVersion,
     },
   },
+  recordTypes: {
+    citation: {
+      vocabularies: {
+        worldcat: {
+          disabled: true,
+        },
+      },
+    },
+    concept: {
+      vocabularies: {
+        material: {
+          disabled: true,
+        },
+        activity: {
+          disabled: true,
+        },
+      },
+    },
+    organization: {
+      vocabularies: {
+        ulan: {
+          disabled: true,
+        },
+      },
+    },
+    location: {
+      vocabularies: {
+        offsite: {
+          disabled: true,
+        },
+      },
+    },
+    person: {
+      vocabularies: {
+        ulan: {
+          disabled: true,
+        },
+      },
+    },
+    place: {
+      vocabularies: {
+        tgn: {
+          disabled: true,
+        },
+      },
+    },
+    work: {
+      vocabularies: {
+        cona: {
+          disabled: true,
+        },
+      },
+    },
+  },
   plugins: plugins.map(plugin => plugin()),
 });
