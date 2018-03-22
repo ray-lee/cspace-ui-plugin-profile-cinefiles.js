fd4fcd7608f058b247aa98937fab223d360cd27c
Cesar Villalobos <cesarv.h@berkeley.edu>
2018-03-22 16:35:36 -0700

    BAMPFA-591: Added works advanced search customizations

---
+++ src/plugins/recordTypes/work/advancedSearch.js
@@ -0,0 +1,75 @@
+export default (pluginContext) => {
+  const {
+    OP_EQ,
+    OP_CONTAIN,
+    OP_RANGE,
+  } = pluginContext.searchOperators;
+
+  const {
+    defaultAdvancedSearchBooleanOp,
+    extensions,
+  } = pluginContext.config;
+
+  return {
+    op: defaultAdvancedSearchBooleanOp,
+    value: [
+      {
+        op: OP_CONTAIN,
+        path: 'ns2:works_common/workTermGroupList/workTermGroup/termDisplayName',
+      },
+      {
+        op: OP_CONTAIN,
+        path: 'ns2:works_common/workTermGroupList/workTermGroup/termName',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:works_common/workTermGroupList/workTermGroup/termLanguage',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:works_common/workTermGroupList/workTermGroup/termStatus',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:works_common/workType',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:works_common/creatorGroupList/creatorGroup/creator',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:works_cinefiles/genres/genre',
+      },
+      {
+        op: OP_RANGE,
+        path: 'ns2:works_common/workDateGroupList/workDateGroup',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:works_cinefiles/countries/country',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:works_cinefiles/languages/language',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:works_common/publisherGroupList/publisherGroup/publisher',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:works_cinefiles/subjects/subject',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:works_cinefiles/themes/theme',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:works_common/workTermGroupList/workTermGroup/termFlag',
+      },
+      ...extensions.core.advancedSearch,
+    ],
+  };
+};
--- src/plugins/recordTypes/work/index.js
+++ src/plugins/recordTypes/work/index.js
@@ -1,9 +1,11 @@
+import advancedSearch from './advancedSearch';
 import fields from './fields';
 import forms from './forms';
 
 export default () => pluginContext => ({
   recordTypes: {
     work: {
+      advancedSearch: advancedSearch(pluginContext),
       fields: fields(pluginContext),
       forms: forms(pluginContext),
     },
