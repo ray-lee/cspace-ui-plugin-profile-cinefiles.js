4d56a00c8c00333df71e010e02b7fd0e4ef1968d
Cesar Villalobos <cesarv.h@berkeley.edu>
2018-04-03 17:06:33 -0700

    BAMPFA-582: Fixed values in the articles shared option list

--- src/plugins/optionLists/shared.js
+++ src/plugins/optionLists/shared.js
@@ -55,7 +55,7 @@ export default () => ({
         'Ein',
         'Eine',
         'El',
-        'El',
+        'El-',
         'En',
         'Et',
         'Ett',
@@ -63,7 +63,7 @@ export default () => ({
         'Het',
         'I',
         'Il',
-        'L',
+        'L\'',
         'La',
         'Las',
         'Le',
@@ -78,190 +78,12 @@ export default () => ({
         'Um',
         'Uma',
         'Un',
-        'Un',
+        'Un\'',
         'Una',
         'Une',
         'Uno',
         'Yr',
       ],
-      messages: defineMessages({
-        A: {
-          id: 'option.articles.A',
-          defaultMessage: 'A',
-        },
-        Al: {
-          id: 'option.articles.Al',
-          defaultMessage: 'Al',
-        },
-        An: {
-          id: 'option.articles.An',
-          defaultMessage: 'An',
-        },
-        As: {
-          id: 'option.articles.As',
-          defaultMessage: 'As',
-        },
-        Az: {
-          id: 'option.articles.Az',
-          defaultMessage: 'Az',
-        },
-        Das: {
-          id: 'option.articles.Das',
-          defaultMessage: 'Das',
-        },
-        De: {
-          id: 'option.articles.De',
-          defaultMessage: 'De',
-        },
-        Den: {
-          id: 'option.articles.Den',
-          defaultMessage: 'Den',
-        },
-        Der: {
-          id: 'option.articles.Der',
-          defaultMessage: 'Der',
-        },
-        Det: {
-          id: 'option.articles.Det',
-          defaultMessage: 'Det',
-        },
-        Die: {
-          id: 'option.articles.Die',
-          defaultMessage: 'Die',
-        },
-        Een: {
-          id: 'option.articles.Een',
-          defaultMessage: 'Een',
-        },
-        Egy: {
-          id: 'option.articles.Egy',
-          defaultMessage: 'Egy',
-        },
-        Ein: {
-          id: 'option.articles.Ein',
-          defaultMessage: 'Ein',
-        },
-        Eine: {
-          id: 'option.articles.Eine',
-          defaultMessage: 'Eine',
-        },
-        El: {
-          id: 'option.articles.El',
-          defaultMessage: 'El',
-        },
-        'El-': {
-          id: 'option.articles.El-',
-          defaultMessage: 'El-',
-        },
-        En: {
-          id: 'option.articles.En',
-          defaultMessage: 'En',
-        },
-        Et: {
-          id: 'option.articles.Et',
-          defaultMessage: 'Et',
-        },
-        Ett: {
-          id: 'option.articles.Ett',
-          defaultMessage: 'Ett',
-        },
-        Gli: {
-          id: 'option.articles.Gli',
-          defaultMessage: 'Gli',
-        },
-        Het: {
-          id: 'option.articles.Het',
-          defaultMessage: 'Het',
-        },
-        I: {
-          id: 'option.articles.I',
-          defaultMessage: 'I',
-        },
-        Il: {
-          id: 'option.articles.Il',
-          defaultMessage: 'Il',
-        },
-        L: {
-          id: 'option.articles.L',
-          defaultMessage: 'L',
-        },
-        La: {
-          id: 'option.articles.La',
-          defaultMessage: 'La',
-        },
-        Las: {
-          id: 'option.articles.Las',
-          defaultMessage: 'Las',
-        },
-        Le: {
-          id: 'option.articles.Le',
-          defaultMessage: 'Le',
-        },
-        Les: {
-          id: 'option.articles.Les',
-          defaultMessage: 'Les',
-        },
-        Lo: {
-          id: 'option.articles.Lo',
-          defaultMessage: 'Lo',
-        },
-        Los: {
-          id: 'option.articles.Los',
-          defaultMessage: 'Los',
-        },
-        O: {
-          id: 'option.articles.O',
-          defaultMessage: 'O',
-        },
-        Os: {
-          id: 'option.articles.Os',
-          defaultMessage: 'Os',
-        },
-        Ta: {
-          id: 'option.articles.Ta',
-          defaultMessage: 'Ta',
-        },
-        The: {
-          id: 'option.articles.The',
-          defaultMessage: 'The',
-        },
-        To: {
-          id: 'option.articles.To',
-          defaultMessage: 'To',
-        },
-        Um: {
-          id: 'option.articles.Um',
-          defaultMessage: 'Um',
-        },
-        Uma: {
-          id: 'option.articles.Uma',
-          defaultMessage: 'Uma',
-        },
-        Un: {
-          id: 'option.articles.Un',
-          defaultMessage: 'Un',
-        },
-        'Un\'': {
-          id: 'option.articles.Un\'',
-          defaultMessage: 'Un\'',
-        },
-        Una: {
-          id: 'option.articles.Una',
-          defaultMessage: 'Una',
-        },
-        Une: {
-          id: 'option.articles.Une',
-          defaultMessage: 'Une',
-        },
-        Uno: {
-          id: 'option.articles.Uno',
-          defaultMessage: 'Uno',
-        },
-        Yr: {
-          id: 'option.articles.Yr',
-          defaultMessage: 'Yr',
-        },
-      }),
     },
   },
 });
