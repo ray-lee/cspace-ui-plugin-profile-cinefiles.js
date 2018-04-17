513aec5598b7b6a1341c43676271f9d7d043168e
Cesar Villalobos <cesarv.h@berkeley.edu>
2018-04-17 02:48:07 -0700

    BAMPFA-583: Fixed numberOfObjects path on advancedSearch.js

--- src/plugins/recordTypes/collectionobject/advancedSearch.js
+++ src/plugins/recordTypes/collectionobject/advancedSearch.js
@@ -48,7 +48,7 @@ export default (pluginContext) => {
       },
       {
         op: OP_RANGE,
-        path: 'ns2:collectionobjects_cinefiles/numberOfObjects',
+        path: 'ns2:collectionobjects_common/numberOfObjects',
       },
       {
         op: OP_EQ,
