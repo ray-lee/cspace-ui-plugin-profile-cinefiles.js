28861d25978c9847cc1f69f80fdcfa40e37e9155
Cesar Villalobos <cesarv.h@berkeley.edu>
2018-05-02 12:32:16 -0700

    BAMPFA-576: Added missing customizations to concept form

---
+++ src/plugins/recordTypes/concept/forms/default.jsx
@@ -0,0 +1,63 @@
+const template = (configContext) => {
+  const {
+    React,
+  } = configContext.lib;
+
+  const {
+    Panel,
+    Row,
+  } = configContext.layoutComponents;
+
+  const {
+    Field,
+    InputTable,
+  } = configContext.recordComponents;
+
+  return (
+    <Field name="document">
+      <Panel name="info" collapsible>
+        <Field name="conceptTermGroupList">
+          <Field name="conceptTermGroup">
+            <Panel>
+              <Row>
+                <Field name="termDisplayName" />
+                <Field name="termName" />
+                <Field name="termQualifier" />
+                <Field name="termStatus" />
+              </Row>
+
+              <Row>
+                <Field name="termType" />
+                <Field name="termFlag" />
+                <Field name="historicalStatus" />
+                <Field name="termLanguage" />
+                <Field name="termPrefForLang" />
+              </Row>
+
+              <InputTable name="termSource">
+                <Field name="termSource" />
+                <Field name="termSourceDetail" />
+                <Field name="termSourceID" />
+                <Field name="termSourceNote" />
+              </InputTable>
+            </Panel>
+          </Field>
+        </Field>
+
+        <InputTable name="scopeNote">
+          <Field name="scopeNote" />
+          <Field name="scopeNoteSource" />
+          <Field name="scopeNoteSourceDetail" />
+        </InputTable>
+      </Panel>
+
+      <Panel name="hierarchy" collapsible collapsed>
+        <Field name="relation-list-item" subpath="rel:relations-common-list" />
+      </Panel>
+    </Field>
+  );
+};
+
+export default configContext => ({
+  template: template(configContext),
+});
--- src/plugins/recordTypes/concept/index.js
+++ src/plugins/recordTypes/concept/index.js
@@ -1,11 +1,13 @@
 import advancedSearch from './advancedSearch';
 import vocabularies from './vocabularies';
+import forms from './forms';
 
 export default () => configContext => ({
   recordTypes: {
     concept: {
       vocabularies,
       advancedSearch: advancedSearch(configContext),
+      forms: forms(configContext),
     },
   },
 });
