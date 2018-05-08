ba7cb73d2cc4ebe1f696d742215d9093839f9b54
Ray Lee <rhlee@berkeley.edu>
2018-05-08 16:04:03 -0700

    BAMPFA-599: Remove unneeded advanced search config.

--- src/plugins/recordTypes/group/index.js
+++ src/plugins/recordTypes/group/index.js
@@ -1,11 +1,9 @@
 import fields from './fields';
-import advancedSearch from './advancedSearch';
 
 export default () => configContext => ({
   recordTypes: {
     group: {
       fields: fields(configContext),
-      advancedSearch: advancedSearch(configContext),
     },
   },
 });
