8e702da5f743d8b213cb7fdbf0419cbcdd6246ec
cesarvh <cesarv.h@berkeley.edu>
2019-08-14 17:30:55 -0700

    CC-730: Added UOC report templates

--- src/plugins/invocables/report/cinefilesUOCApprovalStatus/fields.js
+++ src/plugins/invocables/report/cinefilesUOCApprovalStatus/fields.js
@@ -3,7 +3,7 @@ import { defineMessages } from 'react-intl';
 export default (configContext) => {
   const {
     CompoundInput,
-    TextInput,
+    OptionPickerInput,
   } = configContext.inputComponents;
 
   const {
@@ -17,17 +17,20 @@ export default (configContext) => {
           type: CompoundInput,
         },
       },
-      Requester: {
+      OutputMIME: {
         [config]: {
+          defaultValue: 'application/pdf',
           messages: defineMessages({
             name: {
-              id: 'field.report.cinefilesUOCApprovalStatus.Requester.name',
-              defaultMessage: 'Requester',
+              id: 'field.report.cinefilesUOCApprovalStatus.OutputMIME.name',
+              defaultMessage: 'Output format',
             },
           }),
-          required: true,
           view: {
-            type: TextInput,
+            type: OptionPickerInput,
+            props: {
+              source: 'uocApprovalStatusMIMETypes',
+            },
           },
         },
       },
--- src/plugins/invocables/report/index.js
+++ src/plugins/invocables/report/index.js
@@ -1,5 +1,7 @@
 import cinefilesUOCApprovalStatus from './cinefilesUOCApprovalStatus';
+import cinefilesUOCRequesterbyObject from './cinefilesUOCRequesterbyObject';
 
 export default [
   cinefilesUOCApprovalStatus,
+  cinefilesUOCRequesterbyObject,
 ];
